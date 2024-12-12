de493302bb7751382c9366f804e94bfe604ae762
TenseiShak1 <Tenseisb4@gmail.com>
2024-12-11 19:15:58 -0500

    app.py add

---
+++ app.py
@@ -0,0 +1,10 @@
+import flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hola'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4000, debug=True)
